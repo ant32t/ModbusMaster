07ee34d4c8b09a834a321a227c8b989c876401fe
Philip Reimer <philip@teleoinc.com>
2021-08-11 09:21:59 -0600

    fix buffer size

--- src/ModbusMaster.cpp
+++ src/ModbusMaster.cpp
@@ -837,7 +837,7 @@ uint8_t ModbusMaster::ModbusMasterTransaction(uint8_t u8MBFunction)
             _u16ResponseBuffer[i] = word(u8ModbusADU[2 * i + 4], u8ModbusADU[2 * i + 3]);
           }
           
-          _u8ResponseBufferLength = i;
+          _u8ResponseBufferLength = i + 1;
         }
         
         // in the event of an odd number of bytes, load last byte into zero-padded word
@@ -863,7 +863,7 @@ uint8_t ModbusMaster::ModbusMasterTransaction(uint8_t u8MBFunction)
             _u16ResponseBuffer[i] = word(u8ModbusADU[2 * i + 3], u8ModbusADU[2 * i + 4]);
           }
           
-          _u8ResponseBufferLength = i;
+          _u8ResponseBufferLength = i + 1;
         }
         break;
     }
